b0ade5a86ceec9a1ab67bafdf35ba84917a852d3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-17 17:44:48 +0800

    Add image in hero section

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, render_template, send_file
-
+import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    blobs = [{'width': random.randint(50, 150),
+              'height': random.randint(50, 150),
+              'top': random.randint(0, 100),
+              'left': random.randint(0, 100)} for _ in range(10)]
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', blobs=blobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
